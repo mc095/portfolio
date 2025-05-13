0413050e24e8d977a6fe8ef913310f2321023665
zcat07 <143707195+ganesh9714@users.noreply.github.com>
2025-05-13 10:38:25 +0530

    og-add

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,24 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ganesh",
   description: "my work and experience",
+  openGraph: {
+    title: "Ganesh",
+    description: "my work and experience",
+    images: [
+      {
+        url: '/og/opengraph.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Portfolio Open Graph Image',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ganesh',
+    description: 'my work and experience',
+    images: ['/og/opengraph.jpg'],
+  },
 };
 
 export default function RootLayout({
